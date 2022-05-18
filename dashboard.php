e550cf25c82c089ff92fa8b6a158702cac5a8656
DTDIF <105635029+DTDIF@users.noreply.github.com>
2022-05-18 14:55:18 +0200

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HANubes Dashboard.</title>
+</head>
+<body>
+    <style>
+        h1{
+            color:red;
+        }
+    </style>
+
+<?php echo "<h1>hey</h1"; ?>
+</body>
+</html> 
