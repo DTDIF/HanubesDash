c54e7575fa2a6f3aa682ffc658ab62009b16492c
DTDIF <d.slurink@student.han.nl>
2022-05-19 13:59:28 +0200

    changed url from li to a

--- index.php
+++ index.php
@@ -12,17 +12,17 @@
     <h1>Welkom bij HANubes</h1>
      <ul>
          <h2>susy01</h2>
-        <li href= "portal.dtdif.hanubes">Portainer</li>
+        <li><a href= "portal.dtdif.hanubes">Portainer</a></li>
       </ul> 
       <ul>
           <h2>susy02</h2>
-        <li href="devops.dtdif.hanubes">Jenkins</li>
+        <li><a href="devops.dtdif.hanubes">Jenkins</a></li>
         <li href="">Github</li>
       </ul> 
       <ul>
         <h2>susy03</h2>
-        <li href="monitoring.dtdif.hanubes">Grafana</li>
-        <li href="support.dtdif.hanubes">OSTicket</li>
+        <li><a href="monitoring.dtdif.hanubes">Grafana</a></li>
+        <li><a href="support.dtdif.hanubes">OSTicket</a></li>
       </ul> 
 </body>
 </html> 
