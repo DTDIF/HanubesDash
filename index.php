24d85f7ec95f0de7b5d27f2c74091d89ee3eb20d
DTDIF <d.slurink@student.han.nl>
2022-05-19 13:45:39 +0200

    bruh

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>HANubes Dashboard.</title>
+<title>HANubes</title>
 </head>
 <body>
     <style>
-        h1{
-            color:red;
+        h1 {
+            background-color:red;
         }
     </style>
-
-<?php echo "<h1>hey</h1"; ?>
+    <h1>Welkom bij HANubes</h1>
+     <ul>
+         <h2>susy01</h2>
+        <li href= "portal.dtdif.hanubes">Portainer</li>
+      </ul> 
+      <ul>
+          <h2>susy02</h2>
+        <li href="devops.dtdif.hanubes">Jenkins</li>
+        <li href="">Github</li>
+      </ul> 
+      <ul>
+        <h2>susy03</h2>
+        <li href="monitoring.dtdif.hanubes">Grafana</li>
+        <li href="support.dtdif.hanubes">OSTicket</li>
+      </ul> 
 </body>
 </html> 
