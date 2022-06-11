19003e7ce5c5f6473e51ac8306b70a5bfb55d6b7
DTDIF <105635029+DTDIF@users.noreply.github.com>
2022-06-11 16:25:18 +0200

    Add files via upload
    new design, nog links toevoegen

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HANubes</title>
+</head>
+<body>
+    <style>
+        h1 {
+            background-color:red;
+        }
+    </style>
+    <h1>Welkom bij HANubes</h1>
+     <ul>
+         <h2>susy01</h2>
+        <li><a href= "portal.dtdif.hanubes">Portainer</a></li>
+      </ul> 
+      <ul>
+          <h2>susy02</h2>
+        <li><a href="devops.dtdif.hanubes">Jenkins</a></li>
+        <li href="">Github</li>
+      </ul> 
+      <ul>
+        <h2>susy03</h2>
+        <li><a href="monitoring.dtdif.hanubes">Grafana</a></li>
+        <li><a href="support.dtdif.hanubes">OSTicket</a></li>
+      </ul> 
+</body>
+</html> 
